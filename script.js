b91399e4a54b8b61230eac02cf26251d9b9128b9
jsncorn <jsncourn@gmail.com>
2022-01-05 18:14:54 -0800

    added 3 more questions in the array for now

--- script.js
+++ script.js
@@ -3,5 +3,20 @@ var quizQuestions = [
         questionTitle: 'Which HTML tag creates an unordered list?',
         choices: ['<ul>', '<li>', '<ol>', '<s>'],
         amswer: 'ul'
+    },
+    {
+        questionTitle: 'jQuery is an API extension of what?',
+        choices: ['HTML', 'Bootstrap', 'Javascript', 'CSS'],
+        amswer: 'Javascript'
+    },
+    {
+        questionTitle: 'In Javascript, what is used to store data values?',
+        choices: ['array', 'string', 'function', 'variables'],
+        amswer: 'variables'
+    },
+    {
+        questionTitle: 'How do you NOT declare a variable in Javascript?',
+        choices: ['let', 'var', 'const', '--var'],
+        amswer: '--var'
     }
 ]
